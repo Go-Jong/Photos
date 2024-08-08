bc13081f355080f318b01848a25291635102d152
Go=Jong <tje8746@icloud.com>
2024-08-08 11:58:20 +0800

    my photos

---
+++ js/pictureList.js
@@ -0,0 +1,57 @@
+const PICTURE_SRC_LIST = [
+  {
+    path: './image/photo1.jpeg',
+    title: '절친과 함꼐',
+    subTitle:
+      '친구 만나러 서울까지 올라갔다가 가장 친한 친구와 한강에서 술 마시고 찍은 사진 ㅋㅋ',
+  },
+  {
+    path: './image/photo2.jpeg',
+    title: 'Jeju University',
+    subTitle: '분과에서 퇴근 길에 달이 너무 예뻐보였어...',
+  },
+  {
+    path: './image/photo3.jpeg',
+    title: 'Landscape',
+    subTitle: 'Ex sint consectetur velit deserunt.',
+  },
+  {
+    path: './image/photo4.jpeg',
+    title: 'Landscape',
+    subTitle: 'Eiusmod sint consectetur reprehenderit id.',
+  },
+  {
+    path: './image/photo5.jpeg',
+    title: '절친과 함꼐',
+    subTitle:
+      'He is one of the my best friend. He took my cellphone and took his photo himself. It is so funny!',
+  },
+  {
+    path: './image/photo6.jpeg',
+    title: 'Landscape',
+    subTitle: '수능 준바할 떄 다니던 독서실 옥상에서 자주 보이는 저녁 노을',
+  },
+  {
+    path: './image/photo7.jpeg',
+    title: 'Landscape',
+    subTitle:
+      '언제인지 기억은 나지 않지만 구름이 너무 멋져 보여서 찍은 사진. (필터링으로 약간 어둡게 처리 한거는 비밀 ㅋ)',
+  },
+  {
+    path: './image/photo8.jpeg',
+    title: 'Landscape',
+    subTitle: '언제인지 기억은 나지 않지만 구름이 너무 멋져 보여서 찍은 사진.',
+  },
+  {
+    path: './image/photo9.jpeg',
+    title: '첫 만남이지만....',
+    subTitle: '나 빼고 3명끼리 서로 처음 보는데 술까지 ㅋㅋ',
+  },
+  {
+    path: './image/photo10.jpeg',
+    title: 'Hackerthon Team',
+    subTitle: '해커톤 팀원들과 마지막 회의에서 사진 한장!',
+  },
+]
+
+export default PICTURE_SRC_LIST
